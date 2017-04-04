91efc93b3de7a9cc4bd41a3fa65ed303b06d7ac5
amymaker <littlemamamaker@gmail.com>
2017-04-04 14:47:32 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -1,1 +1,6 @@
 # This is a comment
+pwd     # stands for print working directory (where)
+whoami  # stands for which user I am logged in as (who)
+ls      # tells what files are in directory (what)
+cd      # allows user to change to other directories (navigation)
+date    # what time is it on the machine (when)
